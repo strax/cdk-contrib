48ae6b0a7204ac9bbbc8bb2d9f915b0282a1c998
Sami Kukkonen <sami@samikukkonen.fi>
2019-08-13 00:01:47 +0300

    Add package name to test results

--- packages/testing/bin/cdk-ci-test.sh
+++ packages/testing/bin/cdk-ci-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if has-script "test:tap"; then
-  command npm --silent run-script test:tap | npx --quiet tap-xunit > test-results.xml
+  command npm --silent run-script test:tap | npx --quiet tap-xunit --package $LERNA_PACKAGE_NAME > test-results.xml
   echo "Wrote test results to $(pwd)/test-results.xml"
 fi
