2938cb4fca0216dfabf46f50a5540bbc962248e0
Sami Kukkonen <sami@samikukkonen.fi>
2019-08-17 17:29:51 +0300

    Use GitHub CI instead of Azure Pipelines

--- packages/testing/bin/cdk-ci-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if has-script "test:tap"; then
-  command npm --silent run-script test:tap | npx --quiet tap-xunit --package $LERNA_PACKAGE_NAME > test-results.xml
-  echo "Wrote test results to $(pwd)/test-results.xml"
-fi
--- packages/testing/bin/has-script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-command node -p "Object.keys(require('./package.json').scripts || {}).join('\n')" | grep -q "$1"
